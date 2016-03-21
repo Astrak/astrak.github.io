18c78c9a710a1006a7333edca2139edc25bd8626
Astrak <coffre.a@gmail.com>
2016-03-21 21:52:42 +0100

    testing offline webapp !

---
+++ offline/sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/index.html',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch',function(e){
+	e.respondWith(
+		caches.match(e.request).then( function (res){
+			if ( res ) {
+				console.log('match in cache !', res);
+				return res;
+			}
+			console.log('why do i log')
+		}).catch( function ( err ) {
+			console.log('nothing in cache',err)
+		})
+	);
+});
