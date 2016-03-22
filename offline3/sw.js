52b7c5615fbdb8d91ac70c2bd4a55efd1a4cca44
Astrak <coffre.a@gmail.com>
2016-03-22 09:54:41 +0100

    offline web app 9

--- offline3/sw.js
+++ offline3/sw.js
@@ -8,20 +8,20 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log('Opened cache' + CACHE_NAME );
         return cache.addAll(urlsToCache);
       })
   );
+  console.log('installation complete')
 });
 
 self.addEventListener('fetch',function(e){
+	console.log(e)
 	e.respondWith(
 		caches.match(e.request).then( function (res){
 			if ( res ) return res;
 			console.log('use network')
 			return fetch(e.request);
-		}).catch( function ( err ) {
-			console.log('nothing in cache',err)
-		})
+		});
 	);
 });
