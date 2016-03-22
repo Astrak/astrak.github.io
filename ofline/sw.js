f86d91a33fe7e9199bcd7822180d11bedfecbf82
Astrak <coffre.a@gmail.com>
2016-03-23 00:11:13 +0100

    32

--- ofline/sw.js
+++ ofline/sw.js
@@ -35,14 +35,16 @@ self.addEventListener( 'fetch' , function ( e ) {
 	);
 });
 
-// self.addEventListener( 'activate' , function ( e ) {
-// 	e.waitUntil(
-// 		caches.keys().then( function ( cacheNames ) {
-// 			return Promise.all(
-// 				cacheNames.map( function ( cacheName ) {
-// 					if ( urlsToCache.indexOf( cacheName ) === -1 ) return caches.delete( cacheName );
-// 				})
-// 			)
-// 		})
-// 	);
-// });
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
