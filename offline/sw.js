1009e11babd1e196934833bb1ce31151406d7617
Astrak <coffre.a@gmail.com>
2016-03-22 17:29:43 +0100

    26

--- offline/sw.js
+++ offline/sw.js
@@ -1,13 +1,12 @@
 var CACHE_NAME = 'v2';
 var urlsToCache = [
-  '/index.html',
-  '/sw.js'
+  '/index.html'
 ];
 
 self.addEventListener( 'install' , function ( e ) {
 	e.waitUntil(
 		caches.open( CACHE_NAME ).then( function ( cache ) {
-			console.log('youre in v4')
+			console.log('youre in v5')
 			return cache.addAll( urlsToCache );
 		})
 	);
@@ -21,15 +20,16 @@ self.addEventListener( 'fetch' , function ( e ) {
 			.then( function ( cache ) { 
 				if ( cache ) return cache;
 				return fetch( fetchRequest ).then( 
-						function ( res ) {
-							var cacheRes = res.clone(),
-								retRes = res.clone();
-							caches.open( CACHE_NAME ).then( function ( cache ) {
-									cache.put( fetchRequest, cacheRes );
-								});
-							return retRes;
-						}
-					);
+					function ( res ) {
+						var cacheRes = res.clone(),
+							retRes = res.clone();
+						caches.open( CACHE_NAME ).then( function ( cache ) {
+							console.log('has fetched : '+fetchRequest.clone())
+							cache.put( fetchRequest, cacheRes );
+						});
+						return retRes;
+					}
+				);
 			})
 				
 	);
