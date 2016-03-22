9e144b545b014117fd7c98f4d4e9b00204966358
Astrak <coffre.a@gmail.com>
2016-03-22 16:35:50 +0100

    offline app 15

--- offline/sw2.js
+++ offline/sw2.js
@@ -8,26 +8,38 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        navigator.serviceWorker.controller.postMessage('Opened cache' + CACHE_NAME );
         return cache.addAll(urlsToCache);
       })
   );
-  navigator.serviceWorker.controller.postMessage('installation complete')
 });
 
 self.addEventListener('fetch',function(e){
-	navigator.serviceWorker.controller.postMessage(e)
-	e.respondWith(
-		caches.match(e.request).then( function (res){
-			if ( res ) return res;
-			var req = e.request.clone();
-			return fetch( req ).then( function ( res ) {
-				if ( ! res || res.status !== 200 || res.type !== 'basic' ) return res;
-				var cacheRes = res.clone();
-				caches.open( CACHE_NAME ).then( function( cache ) {
-					cache.put( e.request, cacheRes );
-				})
-			})
-		})
-	);
+	var fetchRequest = e.request.clone(),
+		cacheRequest = e.request.clone();
+
+	e.respondWith( 
+		caches.match( cacheRequest )
+			.then( function ( cache ) { return cache; })
+			.catch( function ( err ) {
+				fetch( fetchRequest )
+					.then( function ( res ) {
+						caches.open( CACHE_NAME ).then( function ( cache ) {
+								cache.put( fetchRequest, res );
+							})
+					})
+			});
 });
+
+	// e.respondWith(
+	// 	caches.match(e.request.clone()).then( function (res){
+	// 		if ( res ) return res;
+	// 		var req = e.request.clone();
+	// 		return fetch( req ).then( function ( res ) {
+	// 			if ( ! res || res.status !== 200 || res.type !== 'basic' ) return res;
+	// 			var cacheRes = res.clone();
+	// 			caches.open( CACHE_NAME ).then( function( cache ) {
+	// 				cache.put( e.request, cacheRes );
+	// 			})
+	// 		})
+	// 	})
+	// );
