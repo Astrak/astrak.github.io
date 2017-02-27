dfc354d9998d6551195ee23fc5722a0b2636a7af
Astrak <coffre.a@gmail.com>
2017-02-27 13:37:02 +0100

    add detector

--- js/webgl.js
+++ js/webgl.js
@@ -142,6 +142,27 @@ function detectWebGL () {
 
 }
 
+function XHR () {
+
+	var xhr;
+
+	if ( window.XMLHttpRequest || window.ActiveXObject ) {
+		if ( window.ActiveXObject ) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+		return xhr;
+	} else {
+		alert( "Votre navigateur n'est pas à jour" );
+	}
+
+}
+
 function upsert ( o ) {
 
 	var xhr = new XHR();
